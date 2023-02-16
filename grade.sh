3493e85d5058d64e5d2888229a3d47d3721c0b87
courtney-lo <clo@ucsd.edu>
2023-02-15 20:51:35 -0800

    ok
    ok

--- grade.sh
+++ grade.sh
@@ -3,3 +3,15 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+cd student-submission
+
+if [[ -e ListExamples.java ]]
+then
+    echo 'Corrent File'
+else
+    echo 'Incorrect File'
+
+mkdir Test-Student
+cp $file > Test-Student
+cp TestListExamples.java > Test-Student
