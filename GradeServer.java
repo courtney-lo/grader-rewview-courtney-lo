93328d17f5c1769a15b46c9ce174187799d5291d
courtney-lo <clo@ucsd.edu>
2023-03-01 13:58:59 -0800

    changes

--- GradeServer.java
+++ GradeServer.java
@@ -1,11 +1,11 @@
-import java.io.BufferedReader;
+// import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+// import java.io.InputStreamReader;
 import java.net.URI;
-import java.net.URISyntaxException;
+// import java.net.URISyntaxException;
 import java.util.Arrays;
-import java.util.stream.Stream;
+// import java.util.stream.Stream;
 
 class ExecHelpers {
 
--- grade.sh
+++ grade.sh
@@ -1,3 +1,27 @@
+# CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+
+# rm -rf student-submission
+# git clone $1 student-submission
+# echo 'Finished cloning'
+
+# cd student-submission
+
+# if [[ -e ListExamples.java ]]
+# then
+#     echo 'Corrent File'
+# else
+#     echo 'Incorrect File'
+# fi
+
+# mkdir Test-Student
+# ls -l
+# cp $file ./Test-Student
+# cp TestListExamples.java ./Test-Student
+
+# cp student-submission/ListExamples.java ./
+# javac -cp $CPATH *.java
+# java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
@@ -8,10 +32,13 @@ cd student-submission
 
 if [[ -e ListExamples.java ]]
 then
-    echo 'Corrent File'
-else
-    echo 'Incorrect File'
+    echo "FIle is existed"
+else 
+    "File is not existed"
+fi
 
-mkdir Test-Student
-cp $file > Test-Student
-cp TestListExamples.java > Test-Student
+cd..
+
+cp student-submission/ListExamples.java ./
+javac -cp $CPATH *.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples 
